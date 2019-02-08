edf183b55216824cdde1647a9bdc50510cd7473a
kashifkhan79 <kashifkhan79@gmail.com>
2019-02-08 15:52:19 -0800

    Added more methods to the controller and service class

--- src/main/java/com/stringcodelab/restfuldemo/controller/actor/ActorController.java
+++ src/main/java/com/stringcodelab/restfuldemo/controller/actor/ActorController.java
@@ -24,16 +24,39 @@ public class ActorController {
         return actorService.getAllActors();
     }
 
-    @RequestMapping(value = "/actor/active", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public Actor getActorWithMostPlayedMovies() {
-        return actorService.getActorWithMostPlayedMovies();
-    }
-
     @RequestMapping(value = "/actor/movies", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public List<Movie> getMoviesForActor(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
         return actorService.getMoviesForActor(firstName, lastName);
     }
 
+    @RequestMapping(value = "/actor/movies/min", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Actor getActorWithLeastPlayedMovies() {
+        return actorService.getActorWithMinimumPlayedMovies();
+    }
+
+    @RequestMapping(value = "/actor/movies/max", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Actor getActorWithMostPlayedMovies() {
+        return actorService.getActorWithMostPlayedMovies();
+    }
+
+    @RequestMapping(value = "/actor/movies/min/year", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Actor getActorWithLeastPlayedMoviesInGivenYear(@RequestParam(value = "value") int value) {
+        return actorService.getActorWithLeastMoviesInGivenYear(value);
+    }
+
+    @RequestMapping(value = "/actor/movies/max/year", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Actor getActorWithMostPlayedMoviesInGivenYear(@RequestParam(value = "value") int value) {
+        return actorService.getActorWithMaxMoviesInGivenYear(value);
+    }
+
+    @RequestMapping(value = "/actor/active/year", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Actor getActorWithMostPlayedMoviesInAnyYear() {
+        return actorService.getActorWithMostMoviesInAnySingleYear();
+    }
 }
--- src/main/java/com/stringcodelab/restfuldemo/service/actor/ActorService.java
+++ src/main/java/com/stringcodelab/restfuldemo/service/actor/ActorService.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,6 +48,20 @@ public class ActorService {
         return moviesByActor;
     }
 
+    public Actor getActorWithMinimumPlayedMovies() {
+        final Map.Entry<Actor, Long> actorWithMinimumMoviesPlayed = movieRepository.getAllMovies()
+                .stream()
+                .flatMap(movie -> movie.getActors().stream())
+                .collect(Collectors.toMap(Function.identity(), actor -> 1L, (c1, c2) -> c1 + c2))
+                .entrySet()
+                .stream()
+                .min(Map.Entry.comparingByValue())
+                .get();
+
+        LOGGER.info(String.format("Actor %s played in the least amount of %d movies", actorWithMinimumMoviesPlayed.getKey().getFullName(), actorWithMinimumMoviesPlayed.getValue()));
+
+        return actorWithMinimumMoviesPlayed.getKey();
+    }
 
     public Actor getActorWithMostPlayedMovies() {
 
@@ -60,4 +79,98 @@ public class ActorService {
         return actorWithMostMoviesPlayed.getKey();
 
     }
+
+    public Actor getActorWithLeastMoviesInGivenYear(int year) {
+        final Optional<Map.Entry<Actor, Long>> optionalEntry = movieRepository.getAllMovies()
+                .stream()
+                .filter(movie -> movie.getReleaseYear() == year)
+                .flatMap(movie -> movie.getActors().stream())
+                .collect(Collectors.toMap(Function.identity(), a -> 1L, (c1, c2) -> c1 + c2))
+                .entrySet()
+                .stream()
+                .min(Map.Entry.comparingByValue());
+
+        Actor actor = null;
+        if(optionalEntry.isPresent()) {
+            actor = optionalEntry.get().getKey();
+
+            LOGGER.info(String.format("Actor %s played in %d movies in the year %d", actor.getFullName(), optionalEntry.get().getValue(), year));
+
+        }
+        return actor;
+    }
+
+    public Actor getActorWithMaxMoviesInGivenYear(int year) {
+        final Optional<Map.Entry<Actor, Long>> optionalEntry = movieRepository.getAllMovies()
+                .stream()
+                .filter(movie -> movie.getReleaseYear() == year)
+                .flatMap(movie -> movie.getActors().stream())
+                .collect(Collectors.toMap(Function.identity(), a -> 1L, (c1, c2) -> c1 + c2))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
+
+        Actor actor = null;
+        if(optionalEntry.isPresent()) {
+            actor = optionalEntry.get().getKey();
+
+            LOGGER.info(String.format("Actor %s played in %d movies in the year %d", actor.getFullName(), optionalEntry.get().getValue(), year));
+
+        }
+        return actor;
+    }
+
+    public Actor getActorWithMostMoviesInAnySingleYear() {
+
+        // Java 8 style.
+        Supplier<Map<Actor, AtomicLong>> supplier = () -> new HashMap<>();
+
+        BiConsumer<Map<Actor, AtomicLong>, Movie> accumulator = (map, movie) ->
+                movie.getActors().forEach(
+                        actor ->
+                                map.computeIfAbsent(
+                                        actor, a-> new AtomicLong()
+                                ).incrementAndGet()
+                );
+
+        BinaryOperator<Map<Actor, AtomicLong>> combiner = (map1, map2) -> {
+            map2.entrySet().forEach(
+                    entry1 -> map1.merge(
+                            entry1.getKey(),
+                            entry1.getValue(),
+                            (al1, al2) -> new AtomicLong(al1.get() + al2.get())
+                    )
+            );
+            return map1;
+        };
+
+        Map.Entry<Integer, Map.Entry<Actor, AtomicLong>> mostProductiveActorInAnyYear = movieRepository.getAllMovies()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Movie::getReleaseYear,
+                                // Hand crafted collector
+                                Collector.of(supplier, accumulator, combiner, Collector.Characteristics.CONCURRENT)
+                        )
+                )
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry3 -> entry3.getKey(),
+                        entry3 -> entry3.getValue().entrySet().stream().max(Comparator.comparing(entry4 -> entry4.getValue().get()))
+                                .get()
+                        )
+                ).entrySet()
+                .stream()
+                .max(Comparator.comparing(entry4 -> entry4.getValue().getValue().get()))
+                .get();
+
+        final Actor actor = mostProductiveActorInAnyYear.getValue().getKey();
+        final int year = mostProductiveActorInAnyYear.getKey();
+        final long moviesPlayed = mostProductiveActorInAnyYear.getValue().getValue().get();
+
+        LOGGER.info(String.format("Actor %s appeared in %d movies in %d year", actor.getFullName(), moviesPlayed, year));
+
+        return actor;
+    }
+
 }
